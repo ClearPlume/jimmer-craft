8c8cd40b66128be5fe525c5e9373256c02b2940f
FallingAngel <the.fallenangel.965@gmail.com>
2026-08-30 20:22:58 +0800

    fix(completion): 自动弹出限定在有候选的注解参数位置

--- src/main/kotlin/net/fallingangel/jimmercraft/completion/JavaCompletionConfidence.kt
+++ src/main/kotlin/net/fallingangel/jimmercraft/completion/JavaCompletionConfidence.kt
@@ -5,13 +5,27 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiNameValuePair
 import com.intellij.util.ThreeState
-import net.fallingangel.jimmerdto.util.haveParent
+import net.fallingangel.jimmercraft.facts.Completions
+import net.fallingangel.jimmercraft.facts.JimmerFacts
+import net.fallingangel.jimmerdto.lsi.process
+import net.fallingangel.jimmerdto.util.parent
 
 class JavaCompletionConfidence : CompletionConfidence() {
     override fun shouldSkipAutopopup(editor: Editor, contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState {
-        return if (contextElement.haveParent<PsiAnnotation>()) {
-            ThreeState.NO
+        val annotation = contextElement.parent<PsiAnnotation>()
+        return if (annotation != null) {
+            val annotationClass = annotation.resolveAnnotationType() ?: return ThreeState.UNSURE
+            val annotationName = process(annotationClass) { className() } ?: return ThreeState.UNSURE
+            val parameter = contextElement.parent<PsiNameValuePair>() ?: return ThreeState.UNSURE
+            val parameterName = parameter.name ?: "value"
+
+            if (JimmerFacts[Completions, annotationName to parameterName] != null) {
+                ThreeState.NO
+            } else {
+                ThreeState.UNSURE
+            }
         } else {
             ThreeState.UNSURE
         }
--- src/main/kotlin/net/fallingangel/jimmercraft/completion/KotlinCompletionConfidence.kt
+++ src/main/kotlin/net/fallingangel/jimmercraft/completion/KotlinCompletionConfidence.kt
@@ -5,13 +5,31 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.util.ThreeState
-import net.fallingangel.jimmerdto.util.haveParent
+import net.fallingangel.jimmercraft.facts.Completions
+import net.fallingangel.jimmercraft.facts.JimmerFacts
+import net.fallingangel.jimmerdto.lsi.process
+import net.fallingangel.jimmerdto.util.parent
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.types.symbol
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtValueArgument
 
 class KotlinCompletionConfidence : CompletionConfidence() {
     override fun shouldSkipAutopopup(editor: Editor, contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState {
-        return if (contextElement.haveParent<KtAnnotationEntry>()) {
-            ThreeState.NO
+        val annotation = contextElement.parent<KtAnnotationEntry>()
+        return if (annotation != null) {
+            val annotationClass = analyze(annotation) {
+                annotation.typeReference?.type?.symbol?.psi ?: return ThreeState.UNSURE
+            }
+            val annotationName = process(annotationClass) { className() } ?: return ThreeState.UNSURE
+            val parameter = contextElement.parent<KtValueArgument>() ?: return ThreeState.UNSURE
+            val parameterName = parameter.getArgumentName()?.asName?.asString() ?: "value"
+
+            if (JimmerFacts[Completions, annotationName to parameterName] != null) {
+                ThreeState.NO
+            } else {
+                ThreeState.UNSURE
+            }
         } else {
             ThreeState.UNSURE
         }
